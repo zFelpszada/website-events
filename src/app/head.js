14e697dd74190da6523ca85d5549923aeb76626c
Felps157 <lipeaugustocastro@gmail.com>
2023-02-16 19:29:43 -0300

    v2

--- src/app/head.js
+++ src/app/head.js
@@ -1,14 +1,10 @@
-'use client';
-
-import NextHead from 'next/head';
-
 export default function Head() {
 	return (
-		<NextHead>
+		<>
 			<title>Desafio 07</title>
 			<meta name="description" content="Desafio 07 rocketseat" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="icon" href="/favicon.ico" />
-		</NextHead>
+		</>
 	);
 }
---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+import '@/styles/globals.css';
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="pt-br">
+			<head>
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+			</head>
+			<body>{children}</body>
+		</html>
+	);
+}
