6079015c0a1d2b43fb269a5ab543b5e530caac22
Felps157 <lipeaugustocastro@gmail.com>
2023-02-16 19:39:59 -0300

    v2

--- src/app/page.js
+++ src/app/page.js
@@ -69,7 +69,7 @@ export default function Home() {
 				</div>
 				<div className="mx-8 max-w-[993px] w-full">
 					<div>
-						<h1 className="text-center text-[#E45858] font-Roboto font-normal">FIND YOUR BLOCK</h1>
+						<h1 className="text-center text-[#A08CFF] font-Roboto font-normal">ACHE SEU LUGAR</h1>
 						<h2 className="mt-4 mx-auto max-w-[647px] text-center text-2xl text-[#121214] font-Roboto font-bold md:text-5xl">
 							Encontre os <span className="text-[#6246EA]">melhores blocos</span> de carnaval de 2023
 						</h2>
@@ -78,11 +78,11 @@ export default function Home() {
 						<form onSubmit={handleSubmit} className="m-10 space-y-5 md:space-y-0 md:flex md:gap-5">
 							<div className="mx-auto w-full h-12 rounded-md bg-[#F4F4FF] flex items-center md:max-w-[348px]">
 								<RxMagnifyingGlass className="mx-2 text-[#E45858]" size={24} />
-								<input ref={search} id="name" name="name" type="text" placeholder="Pesquise por nome" className="w-4/5 bg-transparent text-[#7C7C8A] placeholder-[#7C7C8A] font-Roboto focus:outline-none" />
+								<input ref={search} id="name" name="name" type="text" placeholder="Pesquise por nome" className="w-4/5 bg-transparent text-black placeholder-black font-Roboto focus:outline-none" />
 							</div>
 							<div className="mx-auto w-full h-12 rounded-md bg-[#F4F4FF] flex items-center md:max-w-[348px] relative">
 								<VscLocation className="mx-2 text-[#E45858]" size={30} />
-								<select onChange={changeCity} id="countries" defaultValue="Selecione uma cidade" className="block w-4/5 bg-transparent appearance-none text-[#7C7C8A] placeholder-[#7C7C8A] font-Roboto focus:outline-none focus:ring-0">
+								<select onChange={changeCity} id="countries" defaultValue="Selecione uma cidade" className="block w-4/5 bg-transparent appearance-none text-black placeholder-black font-Roboto focus:outline-none focus:ring-0">
 									<option value="Selecione uma cidade">Selecione uma cidade</option>
 									{cities.map((data, index) => (
 										<option key={index} value={data.name}>
